fd7158b698e8e009af382cb7ff7c40122d64ba5e
Michael Capponi <michael.capponi@progettopa.it>
2023-09-02 12:07:01 +0200

    lezione media-query, evento click js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log('Scemo chi legge 🙁');
+
+var menuEl = document.getElementById("menu");
+const shoppingBagEl = document.getElementById("shopping-bag");
+
+console.log(menuEl);
+var menuShow = false;
+
+function handleMenu() {
+    menuShow = !menuShow;
+    if(menuShow)
+        shoppingBagEl.classList.remove("hide");
+    else {
+        shoppingBagEl.classList.add("hide");
+    }
+}
+
+menuEl.addEventListener("click", handleMenu)
